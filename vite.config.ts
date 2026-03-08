1689535809396e888942144a34c66e95246064a3
Akshat Singh <akshatsingh132004@gmail.com>
2026-03-08 22:40:16 +0530

    Added a allowedHost for testing

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts :['bootlessly-heterolecithal-miya.ngrok-free.dev']
+  }
 })
